8e9f2119d3b37bfca324b88724f0f2637acf26ba
Oristic16 <kanin_champ@hotmail.com>
2023-09-28 17:07:19 +0700

    save

--- src/pages/EntryWork/index.js
+++ src/pages/EntryWork/index.js
@@ -18,73 +18,75 @@ import { useState } from "react";
 import { useEffect } from "react";
 import profileImg from "../../assets/images/profile-img.png";
 import axios from "axios";
-import monthNames from '../../common/data/monthName'
+import monthNames from "../../common/data/monthName";
 import { lastIndexOf } from "lodash";
 import { useMemo } from "react";
 import TableContainer from "../../components/Common/TableContainer";
 
 const EntryWork = () => {
-
-  const colums = useMemo(
-    () => [
-      {
-        Header: "วันที่",
-        accessor: "date"
-      },
-      {
-        Header: "เข้างาน",
-        accessor: "timeIn"
-      },
-      {
-        Header: "ออกงาน",
-        accessor: "timeOut"
-      },
-      {
-        Header: "สถานะ",
-        accessor: "status"
-      },
-      {
-        Header: "หมายเหตุ",
-        accessor: "note"
-      },
-    ]
-  )
+  const colums = useMemo(() => [
+    {
+      Header: "วันที่",
+      accessor: "date",
+    },
+    {
+      Header: "เข้างาน",
+      accessor: "timeIn",
+    },
+    {
+      Header: "ออกงาน",
+      accessor: "timeOut",
+    },
+    {
+      Header: "สถานะ",
+      accessor: "status",
+    },
+    {
+      Header: "หมายเหตุ",
+      accessor: "note",
+    },
+  ]);
 
   const [isEntry, setIsEntry] = useState(false);
-  const [id, setId] = useState(0)
+  const [id, setId] = useState(0);
 
   const handleCheckEntry = () => {
-    axios.get(baseURL + 'getentry')
-    .then((res) => {
-      let lastEleDate = res.data[res.data.length - 1].date;
-      let lastEleTimeIn = res.data[res.data.length - 1].timeIn;
-      let lastEleTimeOut = res.data[res.data.length - 1].timeOut;
-      let idData = res.data[res.data.length - 1].eid
-      console.log(lastEleDate)
-      console.log(idData)
-      setId(idData)
-      if(lastEleDate !== new Date().toLocaleDateString()){
-        return setIsEntry(false)
-      }
-      if(lastEleDate === new Date().toLocaleDateString() && lastEleTimeOut === null){
-        return setIsEntry(true)
-      }
-    }).catch((err) => {
-      console.error(err);
-    })
-  }
+    axios
+      .get(baseURL + "getentry")
+      .then((res) => {
+        let lastEleDate = res.data[res.data.length - 1].date;
+        let lastEleTimeIn = res.data[res.data.length - 1].timeIn;
+        let lastEleTimeOut = res.data[res.data.length - 1].timeOut;
+        let idData = res.data[res.data.length - 1].eid;
+        console.log(lastEleDate);
+        console.log(idData);
+        setId(idData);
+        if (lastEleDate !== new Date().toLocaleDateString()) {
+          return setIsEntry(false);
+        }
+        if (
+          lastEleDate === new Date().toLocaleDateString() &&
+          lastEleTimeOut === null
+        ) {
+          return setIsEntry(true);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   useEffect(() => {
-    handleCheckEntry()
-  },[])
+    handleCheckEntry();
+  }, []);
 
-  const month = new Date().getMonth()
+  const month = new Date().getMonth();
 
-  const baseURL = "http://localhost:8000/"
-  const amountDays = [18,1,0,0]
+  const baseURL = "http://localhost:8000/";
+  const amountDays = [18, 1, 0, 0];
 
-  const [data, setData] = useState([])
-  
+  const [data, setData] = useState([]);
+
   const [timeNow, setTimeNow] = useState(new Date());
 
   const toggleEntry = () => {
@@ -102,144 +104,176 @@ const EntryWork = () => {
   }, []);
 
   const getData = () => {
-    axios.get(baseURL + 'getentry')
-    .then((res) => {
-      setData(res.data)
-      console.log("Entry Data: ",res.data)
-    }).catch((err) => {
-      console.error(err);
-    })
-  }
+    axios
+      .get(baseURL + "getentry")
+      .then((res) => {
+        setData(res.data);
+        console.log("Entry Data: ", res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   useEffect(() => {
     getData();
-  },[]) 
+  }, []);
 
-  
-
-  useEffect(() => {
-  },[])
+  useEffect(() => {}, []);
 
   const handleEntryWork = async (time) => {
-    await axios.post(baseURL + 'addentrywork', {
-      date: time.toLocaleDateString(),
-      timeIn: time.toLocaleTimeString(),
-    }).then(() => {
-      toggleEntry();
-    }).catch((err) => {
-      console.error(err);
-    })
-  }
+    await axios
+      .post(baseURL + "addentrywork", {
+        date: time.toLocaleDateString(),
+        timeIn: time.toLocaleTimeString(),
+      })
+      .then(() => {
+        toggleEntry();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   const handleOutWork = (time, id) => {
-    console.log(id)
-    axios.put(baseURL + `update/${id}`, {
-      timeOut: time.toLocaleTimeString(),
-    }).then(() => {
-      toggleEntry();
-    }).catch((err) => {
-      console.error(err);
-    })
-  }
+    console.log(id);
+    axios
+      .put(baseURL + `update/${id}`, {
+        timeOut: time.toLocaleTimeString(),
+      })
+      .then(() => {
+        toggleEntry();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <div className="page-content">
       <Container fluid>
         <Row>
-          <Col xl={3}>
-            <Card>
-              <CardBody>
-                <FullCalendar
-                  plugins={[BootstrapTheme, dayGridPlugin, interactionPlugin]}
-                  slotDuration={"00:15:00"}
-                  handleWindowResize={true}
-                  themeSystem="bootstrap"
-                  height="420px"
-                  headerToolbar={{
-                    start: "prev",
-                    center: "title",
-                    end: "next",
-                  }}
-                  editable={true}
-                  selectable={true}
-                />
-              </CardBody>
-            </Card>
-          </Col>
-          <Col xl={3}>
-            <Card style={{ minHeight: "460px" }}>
-              <div className="bg-primary bg-soft">
-                <Row>
-                  <Col xs="7">
-                    <div className="text-primary p-4">
-                      <h4 className="text-primary">อรุณสวัสดิ์คุณ .....</h4>
-                      <h6>ยินดีต้อนรับเข้าสู่ สำนักงาน ก.พ.ร</h6>
-                    </div>
-                  </Col>
-                  <Col xs="5" className="align-self-end">
-                    <img src={profileImg} alt="" className="img-fluid" />
-                  </Col>
-                </Row>
-              </div>
-              <CardBody style={{position:'relative'}}>
-                <CardTitle className="text-end">
-                  <h4>ขณะนี้เวลา: {timeNow.toLocaleTimeString()}</h4>
-                </CardTitle>
-
-                <Row>
-                  <Col
-                    style={{ display: "flex", justifyContent: "center" }}
-                  ></Col>
-                  <Col
-                    style={{ display: "flex", justifyContent: "center" }}
-                  ></Col>
-                </Row>
-                {isEntry ?
-                  <Row className="me-2" style={{position:"absolute", bottom: 0, right: 0}}>
-                    <Col>
-                      <p className="text-danger">คุณสามารถออกงานได้ในเวลา</p> 
-                    </Col>
-                  </Row>
-                : null}
-              </CardBody>
-              
-              <CardFooter className="text-end">
-                <Button
-                  className="me-3"
-                  size="lg"
-                  color="success"
-                  disabled={isEntry ? true : false}
-                  onClick={() => {
-                    handleEntryWork(new Date());
-                  }}
-                >
-                  เข้างาน
-                </Button>
-                <Button
-                  size="lg"
-                  color="danger"
-                  disabled={isEntry ? false : true}
-                  onClick={() => {
-                    handleOutWork(new Date(),id);
-                  }}
-                >
-                  ออกงาน
-                </Button>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col xl={6}>
-            <h3 className="me-3">
-            เดือน {monthNames[month]}
-            </h3>
-            <hr className="mt-2" />
+          <Col xxl={7}>
             <Row>
-              <Col>
+              <Col xl={6} xxl={6}>
+                <Card>
+                  <CardBody>
+                    <FullCalendar
+                      plugins={[
+                        BootstrapTheme,
+                        dayGridPlugin,
+                        interactionPlugin,
+                      ]}
+                      slotDuration={"00:15:00"}
+                      handleWindowResize={true}
+                      themeSystem="bootstrap"
+                      height="420px"
+                      headerToolbar={{
+                        start: "prev",
+                        center: "title",
+                        end: "next",
+                      }}
+                      editable={true}
+                      selectable={true}
+                    />
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col xl={6} xxl={6}>
+                <Card style={{ minHeight: "460px" }}>
+                  <div className="bg-primary bg-soft">
+                    <Row>
+                      <Col xs="8">
+                        <div className="text-primary p-4">
+                          <h4 className="text-primary" style={{fontFamily:"Kanit"}}>อรุณสวัสดิ์คุณ .....</h4>
+                          <h6>ยินดีต้อนรับเข้าสู่ สำนักงาน ก.พ.ร</h6>
+                        </div>
+                      </Col>
+                      <Col xs="4" className="align-self-end">
+                        <img src={profileImg} alt="" className="img-fluid" />
+                      </Col>
+                    </Row>
+                  </div>
+                  <CardBody style={{ position: "relative" }}>
+                    <CardTitle className="text-end">
+                      {/* <h4>ขณะนี้เวลา: {timeNow.toLocaleTimeString()}</h4> */}
+                    </CardTitle>
+
+                    <Row>
+                      <Col
+                        style={{ display: "flex", justifyContent: "center" }}
+                      ></Col>
+                      <Col
+                        style={{ display: "flex", justifyContent: "center" }}
+                      ></Col>
+                    </Row>
+                    {isEntry ? (
+                      <Row
+                        className="me-2"
+                        style={{ position: "absolute", bottom: 0, right: 0 }}
+                      >
+                        <Col>
+                          <h4 style={{color:"rgba(255,100,100,0.8)",fontFamily:"Noto Sans Thai"}}>
+                            คุณสามารถออกงานได้ในเวลา +8 ชั่วโมง
+                          </h4>
+                        </Col>
+                      </Row>
+                    ) : null}
+                  </CardBody>
+
+                  <CardFooter className="text-end">
+                    <Button
+                      className="me-3"
+                      size="lg"
+                      color="success"
+                      disabled={isEntry ? true : false}
+                      onClick={() => {
+                        handleEntryWork(new Date());
+                      }}
+                    >
+                      เข้างาน
+                    </Button>
+                    <Button
+                      size="lg"
+                      color="danger"
+                      disabled={isEntry ? false : true}
+                      onClick={() => {
+                        handleOutWork(new Date(), id);
+                      }}
+                    >
+                      ออกงาน
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            </Row>
+            <Row >
+              <Col xxl={12} >
+                <Card>
+                  <CardBody className="pb-3">
+                    <h1>Hello Pete</h1>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+          <Col xxl={5}>
+            <Row>
+              <Col xl={6} xxl={12}>
+                <h3 className="me-3" style={{fontFamily:"Noto Sans Thai"}}>เดือน {monthNames[month]}</h3>
+                <hr className="mt-2" />
                 <Card>
                   <CardBody className="pb-0">
                     <Row>
-                      <Col xl={6} xxl={3}>
-                        <Card style={{display:"flex",alignItems:"center",justifyContent:"center"}} className="bg-success bg-gradient p-5">
+                      <Col xl={6} xxl={3} lg={3} xs={6} sm={3}>
+                        <Card
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                          className="bg-success bg-gradient p-2"
+                        >
                           <Row>
                             <Col>
                               <h4>เต็มเวลา</h4>
@@ -252,8 +286,15 @@ const EntryWork = () => {
                           </Row>
                         </Card>
                       </Col>
-                      <Col xl={6} xxl={3}>
-                        <Card style={{display:"flex",alignItems:"center",justifyContent:"center"}} className="bg-danger bg-gradient p-5">
+                      <Col xl={6} xxl={3} lg={3} xs={6} sm={3}>
+                        <Card
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                          className="bg-danger bg-gradient p-2"
+                        >
                           <Row>
                             <Col>
                               <h4>ขาด</h4>
@@ -266,8 +307,15 @@ const EntryWork = () => {
                           </Row>
                         </Card>
                       </Col>
-                      <Col xl={6} xxl={3}>
-                        <Card style={{display:"flex",alignItems:"center",justifyContent:"center"}} className="bg-success bg-gradient p-5">
+                      <Col xl={6} xxl={3} lg={3} xs={6} sm={3}>
+                        <Card
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                          className="bg-success bg-gradient p-2"
+                        >
                           <Row>
                             <Col>
                               <h4>ครึ่งวัน</h4>
@@ -280,8 +328,15 @@ const EntryWork = () => {
                           </Row>
                         </Card>
                       </Col>
-                      <Col xl={6} xxl={3}>
-                        <Card style={{display:"flex",alignItems:"center",justifyContent:"center"}} className="bg-danger bg-gradient p-5">
+                      <Col xl={6} xxl={3} lg={3} xs={6} sm={3}>
+                        <Card
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                          className="bg-danger bg-gradient p-2"
+                        >
                           <Row>
                             <Col>
                               <h4>สาย</h4>
@@ -297,10 +352,6 @@ const EntryWork = () => {
                     </Row>
                   </CardBody>
                 </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
                 <Card style={{ minHeight: "180px" }}>
                   <CardBody>
                     <TableContainer
@@ -312,13 +363,31 @@ const EntryWork = () => {
                 </Card>
               </Col>
             </Row>
+            <Row>
+              <Col xxl={6}>
+                {/* <Card style={{ minHeight: "180px" }}>
+                  <CardBody>
+                    <TableContainer
+                      columns={colums}
+                      data={data}
+                      customPageSize={5}
+                    />
+                  </CardBody>
+                </Card> */}
+              </Col>
+            </Row>
           </Col>
-          <Col xl={4}>
+          <Col xxl={4}>
+            <Row>
+            <Col xl={12}>
             <Card style={{ minHeight: "400px" }}>
-              <CardBody>
-              </CardBody>
+              <CardBody></CardBody>
             </Card>
           </Col>
+            </Row>
+          </Col>
+          
+          
           <Col xl={5}>
             <Card style={{ minHeight: "400px" }}>
               <CardBody></CardBody>
